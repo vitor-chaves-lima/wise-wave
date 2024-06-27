1843ff0d12dee9038d0f970114f6da70ca57aee5
Vitor Chaves <vitorclcoelho@protonmail.com>
2024-06-26 23:46:41 -0300

    Atualizando script de verificação de diretórios

--- scripts/check_changes_outside_folder.sh
+++ scripts/check_changes_outside_folder.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Verifica se foi passado um diretório como argumento
+# Check if a directory argument is passed
 if [ $# -eq 0 ]; then
   echo "Usage: $0 <directory>"
   exit 1
@@ -8,9 +8,23 @@ fi
 
 directory="$1"
 
-# Verificar se houve alterações fora do diretório especificado
-if git diff --name-only "${GITHUB_BASE_REF}" "${GITHUB_HEAD_REF}" | grep -v "^${directory}/" | grep -q '^'; then
-  echo "Changes outside '${directory}' context detected. Please always work with one context per PR"
+# Variable to track if changes outside specified directory are detected
+changes_detected=false
+changed_dirs=""
+
+# Loop through modified files to check directories
+git diff --name-only "${GITHUB_BASE_REF}" "${GITHUB_HEAD_REF}" | while read -r file; do
+  file_dir=$(dirname "${file}")
+  if [[ "$file_dir" != "$directory" && ! "$changed_dirs" =~ "$file_dir" ]]; then
+    changes_detected=true
+    changed_dirs+=" $file_dir"
+  fi
+done
+
+# If changes outside specified directory are detected, display error message
+if [ "$changes_detected" = true ]; then
+  echo "Changes detected outside '${directory}' context in directories:${changed_dirs}"
+  echo "Please create a separate PR for changes in these directories."
   exit 1
 else
   echo "No changes outside '${directory}'."
