cd6193905526ecf02b3ae7e817ea951d28691ca7
Vitor Chaves <vitorclcoelho@protonmail.com>
2024-06-26 23:55:35 -0300

    Atualizando check_changes_outside_folder.sh

--- scripts/check_changes_outside_folder.sh
+++ scripts/check_changes_outside_folder.sh
@@ -1,22 +1,3 @@
 #!/bin/bash
 
-# Variável para armazenar os diretórios modificados
-changed_dirs=""
-
-# Obtém os arquivos modificados na PR
-files_changed=$(git diff --name-only "${GITHUB_BASE_REF}" "${GITHUB_HEAD_REF}")
-
-# Loop pelos arquivos modificados para extrair diretórios únicos na raiz do repositório
-for file in $files_changed; do
-  dir=$(dirname "$file")
-  # Extrai o diretório raiz do repositório
-  root_dir=$(echo "$dir" | cut -d'/' -f1)
-  # Adiciona o diretório raiz na lista, se ainda não estiver presente
-  if [[ ! "$changed_dirs" =~ "$root_dir" ]]; then
-    changed_dirs+=" $root_dir"
-  fi
-done
-
-# Exibe os diretórios modificados na raiz do repositório
 echo "Directories in the root with changes:"
-echo "$changed_dirs"
