dc3b41409068d546f86b8f51e02b8a9d567cc331
Vitor Chaves <vitorclcoelho@protonmail.com>
2024-06-27 00:16:24 -0300

    Atualizando check_changes_outside_folder.sh

--- scripts/check_changes_outside_folder.sh
+++ scripts/check_changes_outside_folder.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-GITHUB_BASE_REF=${GITHUB_BASE_REF:-$(git symbolic-ref --short HEAD)}
-GITHUB_HEAD_REF=${GITHUB_HEAD_REF:-$(git merge-base HEAD HEAD^)}
-
 echo "Base branch: $GITHUB_BASE_REF"
 echo "Head branch: $GITHUB_HEAD_REF"
 
