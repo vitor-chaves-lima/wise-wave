cdfa6c6d213cb98ef5c7ff1c04ca56b473412b18
Vitor Chaves <vitorclcoelho@protonmail.com>
2024-06-26 23:53:24 -0300

    Testando o check boundaries

--- scripts/check_changes_outside_folder.sh
+++ scripts/check_changes_outside_folder.sh
@@ -1,45 +1,22 @@
 #!/bin/bash
 
-# Check if a directory argument is passed
-if [ $# -eq 0 ]; then
-  echo "Usage: $0 <directory>"
-  exit 1
-fi
-
-directory="$1"
-
-# Variable to track if changes outside specified directory are detected
-changes_detected=false
+# Variável para armazenar os diretórios modificados
 changed_dirs=""
 
-# Get the commit range for comparison
-GITHUB_BASE_REF=${GITHUB_BASE_REF:-$(git merge-base HEAD HEAD^)}
+# Obtém os arquivos modificados na PR
+files_changed=$(git diff --name-only "${GITHUB_BASE_REF}" "${GITHUB_HEAD_REF}")
 
-# Loop through modified files to check directories and list changed directories
-echo "Modified directories:"
-git diff --name-only "$GITHUB_BASE_REF" HEAD | while read -r file; do
-  file_dir=$(dirname "${file}")
-  if [[ ! "$changed_dirs" =~ "$file_dir" ]]; then
-    changed_dirs+=" $file_dir"
-    echo "$file_dir"
-  fi
-done
-echo ""
-
-# Check if changes outside specified directory are detected
-for dir in $changed_dirs; do
-  if [[ "$dir" != "$directory" ]]; then
-    changes_detected=true
+# Loop pelos arquivos modificados para extrair diretórios únicos na raiz do repositório
+for file in $files_changed; do
+  dir=$(dirname "$file")
+  # Extrai o diretório raiz do repositório
+  root_dir=$(echo "$dir" | cut -d'/' -f1)
+  # Adiciona o diretório raiz na lista, se ainda não estiver presente
+  if [[ ! "$changed_dirs" =~ "$root_dir" ]]; then
+    changed_dirs+=" $root_dir"
   fi
 done
 
-# If changes outside specified directory are detected, display error message
-if [ "$changes_detected" = true ]; then
-  echo "Changes detected outside '${directory}' context in directories:"
-  echo "$changed_dirs"
-  echo ""
-  echo "Please create a separate PR for changes in these directories."
-  exit 1
-else
-  echo "No changes outside '${directory}'."
-fi
+# Exibe os diretórios modificados na raiz do repositório
+echo "Directories in the root with changes:"
+echo "$changed_dirs"
