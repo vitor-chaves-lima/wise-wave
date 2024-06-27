4d2b6639a3b443ae6868ebe4f5ae23b6213c943b
Vitor Chaves <vitorclcoelho@protonmail.com>
2024-06-27 00:35:55 -0300

    Atualizando check_changes_outside_folder.sh

--- scripts/check_changes_outside_folder.sh
+++ scripts/check_changes_outside_folder.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 
-# Definir a referência de base da PR
-GITHUB_BASE_REF=${GITHUB_BASE_REF:-$(git symbolic-ref --short HEAD)}
-
-# Obtém o commit base da PR
-GITHUB_BASE_SHA=$(git merge-base "${GITHUB_BASE_REF}" HEAD)
-
-# Obter arquivos modificados na PR
-files_changed=$(git diff --name-only "${GITHUB_BASE_SHA}" HEAD)
+# Obter arquivos modificados na PR usando GitHub environment variables
+files_changed=$(git diff --name-only "${{ github.event.pull_request.base.sha }}" "${{ github.sha }}")
 
 # Exibir os arquivos modificados
 echo "Changed files in the PR:"
