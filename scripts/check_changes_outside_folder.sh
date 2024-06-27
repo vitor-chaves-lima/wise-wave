655f34d2a9df86cc0fe97558f70b75687fb9fc03
Vitor Chaves <vitorclcoelho@protonmail.com>
2024-06-27 00:09:36 -0300

    Atualizando check_changes_outside_folder.sh

--- scripts/check_changes_outside_folder.sh
+++ scripts/check_changes_outside_folder.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-# Definir as referências padrão, se não estiverem definidas
 GITHUB_BASE_REF=${GITHUB_BASE_REF:-$(git symbolic-ref --short HEAD)}
 GITHUB_HEAD_REF=${GITHUB_HEAD_REF:-$(git merge-base HEAD HEAD^)}
 
 echo "Base branch: $GITHUB_BASE_REF"
 echo "Head branch: $GITHUB_HEAD_REF"
+
+# Obter arquivos modificados na PR
+files_changed="$(git diff --name-only "${GITHUB_BASE_REF}" "${GITHUB_HEAD_REF}")"
+
+# Exibir os arquivos modificados
+echo "Changed files in the PR:"
+echo "$files_changed"
