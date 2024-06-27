1d263af05a04a13ad59f48e040f76dee31ca0b0a
Vitor Chaves <vitorclcoelho@protonmail.com>
2024-06-27 00:25:32 -0300

    Atualizando check_changes_outside_folder.sh

--- scripts/check_changes_outside_folder.sh
+++ scripts/check_changes_outside_folder.sh
@@ -4,4 +4,4 @@ echo "Base branch: $GITHUB_BASE_REF"
 echo "Head branch: $GITHUB_HEAD_REF"
 
 # Obter arquivos modificados na PR
-git diff --name-only ${GITHUB_BASE_REF} ${GITHUB_HEAD_REF}
+echo git branch
