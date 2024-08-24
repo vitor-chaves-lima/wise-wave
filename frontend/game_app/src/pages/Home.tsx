19aa7825da22d8613845e2599eaccc5f64f8a61c
Ana Clara Remotto <121055492+anaclaraguimaraesremotto@users.noreply.github.com>
2024-08-24 19:23:22 -0300

    Estilização home page

--- frontend/game_app/src/pages/Home.tsx
+++ frontend/game_app/src/pages/Home.tsx
@@ -1,18 +1,25 @@
-import { Link } from "react-router-dom";
 import Button from 'react-bootstrap/Button';
+import { Link } from "react-router-dom";
+import logo from './../image/ww-home-logo.png';
 
 /*========== MAIN COMPONENT ==========*/
 
 const HomePage = () => {
 
     return (
-        <div className="d-flex justify-content-md-center align-items-center vh-100">
-            <div className='d-flex flex-column gap-5 justify-content-md-center'>
-                <h1 className="mx-auto">Seja bem-vindo!</h1>
-
-                <Link to={"access"} ><Button variant="primary" className="w-100">Iniciar</Button></Link>
+        <div className="d-flex justify-content-center align-items-center vh-100 bg-blue-900">
+            <div className="d-flex flex-column gap-5 text-center">
+                <div>
+                    <img src={logo} alt="logo wise wave" className="mx-auto" />
+                    <h1 className="text-light fs-1-custom text-logo-ww">WiseWave</h1>
+                </div>
+                <h2 className="fs-3 text-light">Seja bem-vindo!</h2>
+                <Link to={"access"}>
+                    <Button variant="primary" size="lg" className="w-100 bg-bt-blue-500">Iniciar</Button>
+                </Link>
             </div>
         </div>
+
     );
 };
 
