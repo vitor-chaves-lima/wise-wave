6ea9997a61273a72b5e7eaecef1b5f2a319d58e6
Ana Clara Remotto <121055492+anaclaraguimaraesremotto@users.noreply.github.com>
2024-08-26 14:56:24 -0300

    WW-30: Página de confirmação de e-mail  (#15)
    * Test
    
    * corrigindo package.json
    
    * Estilização home page
    
    * Access Page
    
    * Access Confirm
    
    ---------
    
    Co-authored-by: Vitor Coelho <vitor.coelho@skf.com>
    Co-authored-by: Vitor Chaves <vitorclcoelho@protonmail.com>

--- frontend/game_app/src/pages/Access.tsx
+++ frontend/game_app/src/pages/Access.tsx
@@ -2,6 +2,7 @@ import { useState, FormEvent } from "react";
 import { Link, useSubmit, useNavigate } from "react-router-dom";
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import returnIcon from './../image/seta-esquerda.png';
 
 /*========== MAIN COMPONENT ==========*/
 
@@ -30,24 +31,39 @@ const AccessPage = () => {
     }
 
     return (
-        <div className="d-flex justify-content-md-center align-items-center vh-100">
-            <div className='d-flex flex-column gap-5 justify-content-md-center'>
-                <Link to={"/"} ><Button variant="primary" className="w-100">Voltar</Button></Link>
+        <div className="d-flex justify-content-center align-items-center vh-100 bg-blue-900">
+        <div className='position-relative' style={{ width: '100%', height: '100%' }}>
+          <Link to={"/"}>
+            <Button variant="link" className="position-absolute start-0 mt-costum-return ms-3">
+              <img src={returnIcon} alt="Ícone de retorno" />
+            </Button>
+          </Link>
+      
+          <div className="d-flex flex-column gap-5 justify-content-md-center align-items-center" style={{ width: '80%', margin: '0 auto' }}>
+            <Form onSubmit={onFormSubmit} className="w-100 mt-costum">
+              <h1 className="mx-auto text-light mt-4">Acesse</h1>
+              <p className="mx-auto text-light fs-5">Utilizando seu e-mail</p>
+      
+              <Form.Group className="mb-3" controlId="email">
+                <Form.Label htmlFor="inputEmail" className="text-light ">Email</Form.Label>
+                <Form.Control
+                  type="email" placeholder="Digite seu e-mail" onChange={onEmailChange} value={email} id="inputEmail"
+                  size="lg"
+                  
+                />
+              </Form.Group>
+      
+              <Button variant="primary" type="submit" size="lg" className="w-100 bg-bt-blue-500">
+                Acessar
+              </Button>
+            </Form>
+          </div>
+        </div>
+      </div>
+      
 
-                <Form onSubmit={onFormSubmit}>
-                    <h1 className="mx-auto">Acesse</h1>
-                    <p className="mx-auto">Utilizando seu e-mail</p>
 
-                    <Form.Group className="mb-3" controlId="email">
-                        <Form.Control type="email" placeholder="Digite seu e-mail" onChange={onEmailChange} value={email} />
-                    </Form.Group>
-                    <Button variant="primary" type="submit" className="w-100">
-                        Acessar
-                    </Button>
 
-                </Form>
-            </div>
-        </div >
     )
 }
 
--- frontend/game_app/src/pages/AccessConfirm.tsx
+++ frontend/game_app/src/pages/AccessConfirm.tsx
@@ -1,4 +1,9 @@
 import { useLocation } from "react-router-dom";
+import returnIconBlue from './../image/seta-esquerda-blue-700.png';
+import { Link } from "react-router-dom";
+import { Button } from "react-bootstrap";
+import logo from './../image/ww-home-logo.png';
+import confirm from './../image/circulo-confirm.png'
 
 /*========== MAIN COMPONENT ==========*/
 
@@ -14,13 +19,28 @@ const AccessConfirmPage = () => {
     const obfuscatedEmail = obfuscateEmail(email);
 
     return (
-        <div className="d-flex justify-content-md-center align-items-center vh-100">
-            <div className='d-flex flex-column gap-5 justify-content-md-center'>
-                <h1 className="mx-auto">Confirme o seu e-mail!</h1>
-                <h2>Você está a um passo de ter a melhor experiência de aprendizagem!</h2>
-                <h3>{obfuscatedEmail}</h3>
+        <div className="d-flex justify-content-center align-items-center vh-100 bg-light">
+            <div className='position-relative' style={{ width: '100%', height: '100%' }}>
+                <Link to={"/access"}>
+                    <Button variant="link" className="position-absolute start-0 mt-costum-return ms-3">
+                        <img src={returnIconBlue} alt="Ícone de retorno" />
+                    </Button>
+                </Link>
+                <div className='d-flex flex-column gap-5 justify-content-center align-items-center w-100' >
+                    <div className="d-flex flex-column gap-2 justify-content-center align-items-center w-100 mt-costum-var2">
+                        <img src={confirm} alt="Imagem de confirmação" style={{ width: '150px', height: '150px' }} />
+                        <h2 className="text-blue-700 fs-1 fw-bold">Confirme o seu e-mail!</h2>
+                        <h3 className="text-blue-900  text-center fs-4">Você está a um passo de ter a melhor experiência de aprendizado!</h3>
+                    </div>
+                    <div className="d-flex flex-column gap- justify-content-center align-items-center w-100">
+                        <h3 className="text-blue-900 fs-5">{obfuscatedEmail}</h3>
+                        <img src={logo} alt="Logo Wise-Wave" />
+                    </div>
+                </div>
             </div>
         </div>
+
+
     );
 };
 
