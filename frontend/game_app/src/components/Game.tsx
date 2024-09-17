4f3ea4d20bb70d2e59dddab20c503a9abbf243b4
Vitor Chaves <vitorclcoelho@protonmail.com>
2024-09-17 14:36:10 -0300

    Finalizando primeira versão do componente game

--- frontend/game_app/src/components/Game.tsx
+++ frontend/game_app/src/components/Game.tsx
@@ -1,11 +1,48 @@
-import { Stage } from "@pixi/react";
+import { Container, Sprite, Stage } from "@pixi/react";
+
+import { useState } from "react";
 import { Button, Row } from "react-bootstrap";
+
 import useWindowDimensions from "../hooks/windowDimensions";
+
+
+/*=============== TYPE ===============*/
+
+export enum TileType {
+    GROUND = "assets/ground.png",
+    WALL = "assets/wall.png",
+    WATER = "assets/water.png",
+}
+
+export enum ActionType {
+    WIN,
+    LOSE,
+    NONE
+}
+
+type Row<T> = [T, T, T, T, T, T, T, T];
+type Matrix8x8<T> = [Row<T>, Row<T>, Row<T>, Row<T>, Row<T>, Row<T>, Row<T>, Row<T>];
+export type TileMap = Matrix8x8<TileType>;
+export type ActionsMap = Matrix8x8<ActionType>;
 
 /*============ INTERFACES ============*/
 
+interface GameProps {
+    levelTitle: string;
+    maps: {
+        gameTileMap: TileMap;
+        actionsMap: ActionsMap;
+    }
+    totalActions: number;
+}
+
 interface GameHeaderProps {
     levelTitle: string;
+}
+
+interface GameStageProps {
+    gameTileMap: TileMap;
+    actionsMap: ActionsMap;
 }
 
 interface GameActionsProps {
@@ -21,36 +58,65 @@ const GameHeader = ({ levelTitle }: GameHeaderProps) => {
     </div>
 }
 
-const GameStage = () => {
+const GameStage = ({ gameTileMap, actionsMap }: GameStageProps) => {
     const { height } = useWindowDimensions();
 
     const gameHeight = (height / 2) - 68;
     const gameWidth = gameHeight;
 
+    const gameTileHeight = gameHeight / 8;
+    const gameTileWidth = gameTileHeight;
+
     return (
-        <Row className="game-stage-custom p-0 m-0 justify-content-center">
-            <Stage height={gameHeight} width={gameWidth} options={{ background: 0x145EA8, resolution: 1 }} className="p-0" >
-            </Stage>
-        </Row>
+        <Stage height={gameHeight} width={gameWidth} options={{ backgroundColor: 0x145EA8, resolution: 1 }} className="p-0">
+            <Container zIndex={2}>
+                {gameTileMap.map((row, rowIndex) => {
+                    return row.map((tile, tileIndex) => {
+                        const x = tileIndex * gameTileWidth;
+                        const y = rowIndex * gameTileHeight;
+
+                        return <Sprite key={`${rowIndex}-${tileIndex}`} x={x} y={y} image={tile} width={gameTileWidth} height={gameTileHeight} />
+                    });
+                })}
+
+            </Container>
+
+            <Container zIndex={1}>
+                {actionsMap.map((row, rowIndex) => {
+                    return row.map((tile, tileIndex) => {
+                        const x = tileIndex * gameTileWidth;
+                        const y = rowIndex * gameTileHeight;
+
+                        if (tile === ActionType.NONE) return null;
+
+                        return <Sprite key={`${rowIndex}-${tileIndex}`} x={x} y={y} image={'assets/chest.png'} width={gameTileWidth} height={gameTileHeight} />
+                    });
+                })}
+
+            </Container>
+        </Stage>
     );
 }
 
 const GameActions = ({ remainingActions, totalActions }: GameActionsProps) => {
+    const buttonsDisabled = remainingActions === 0;
+    const canRestart = !(remainingActions < totalActions);
+
     return (
         <div className="d-flex flex-column actions-custom">
             <div className="d-flex m-0 p-2 bg-blue-900 text-white justify-content-between fs-2-custom">
                 Ações: {remainingActions} / {totalActions}
 
-                <Button disabled={true} variant="danger">Reiniciar</Button>
+                <Button disabled={canRestart} variant="danger">Reiniciar</Button>
             </div>
 
             <div className="d-flex flex-column align-items-center justify-content-center actions-custom">
-                <Button className="mb-2 actions-btn-custom">↑</Button>
+                <Button disabled={buttonsDisabled} className="mb-2 actions-btn-custom">↑</Button>
                 <div className="d-flex actions-side-buttons-custom">
-                    <Button className="actions-btn-custom">←</Button>
-                    <Button className="actions-btn-custom">→</Button>
+                    <Button disabled={buttonsDisabled} className="actions-btn-custom">←</Button>
+                    <Button disabled={buttonsDisabled} className="actions-btn-custom">→</Button>
                 </div>
-                <Button className="mt-2 actions-btn-custom">↓</Button>
+                <Button disabled={buttonsDisabled} className="mt-2 actions-btn-custom">↓</Button>
             </div>
         </div>
     )
@@ -58,16 +124,18 @@ const GameActions = ({ remainingActions, totalActions }: GameActionsProps) => {
 
 /*========== MAIN COMPONENT ==========*/
 
-const GameComponent = () => {
+const GameComponent = ({ levelTitle, totalActions, maps }: GameProps) => {
+    const [remainingActions, _] = useState<number>(totalActions);
+
     return (
         <div className="d-flex flex-column vh-100">
-            <GameHeader levelTitle="Fase 1" />
+            <GameHeader levelTitle={levelTitle} />
 
-            <div className="m-0 p-0 bg-bt-gry-100">
-                <GameStage />
+            <div className="m-0 p-0 d-flex bg-bt-gry-100 game-stage-custom p-0 m-0 justify-content-center">
+                <GameStage gameTileMap={maps.gameTileMap} actionsMap={maps.actionsMap} />
             </div>
 
-            <GameActions remainingActions={3} totalActions={3} />
+            <GameActions remainingActions={remainingActions} totalActions={totalActions} />
         </div>
     );
 };
--- frontend/game_app/src/pages/GameExample.tsx
+++ frontend/game_app/src/pages/GameExample.tsx
@@ -1,12 +1,37 @@
-import GameComponent from '../components/Game';
+import GameComponent, { ActionsMap, ActionType, TileMap, TileType } from '../components/Game';
 
 /*========== MAIN COMPONENT ==========*/
 
 const GameExamplePage = () => {
 
+    const gameTileMap: TileMap = [
+        [TileType.WALL, TileType.WALL, TileType.WATER, TileType.WATER, TileType.GROUND, TileType.GROUND, TileType.GROUND, TileType.GROUND],
+        [TileType.WALL, TileType.WALL, TileType.WATER, TileType.WATER, TileType.GROUND, TileType.GROUND, TileType.GROUND, TileType.GROUND],
+        [TileType.GROUND, TileType.WALL, TileType.WATER, TileType.WATER, TileType.GROUND, TileType.GROUND, TileType.GROUND, TileType.GROUND],
+        [TileType.GROUND, TileType.WALL, TileType.WATER, TileType.WATER, TileType.GROUND, TileType.GROUND, TileType.GROUND, TileType.GROUND],
+        [TileType.GROUND, TileType.WALL, TileType.WATER, TileType.WATER, TileType.GROUND, TileType.GROUND, TileType.GROUND, TileType.GROUND],
+        [TileType.GROUND, TileType.WALL, TileType.GROUND, TileType.GROUND, TileType.GROUND, TileType.GROUND, TileType.GROUND, TileType.GROUND],
+        [TileType.GROUND, TileType.GROUND, TileType.GROUND, TileType.GROUND, TileType.GROUND, TileType.GROUND, TileType.GROUND, TileType.GROUND],
+        [TileType.GROUND, TileType.GROUND, TileType.GROUND, TileType.GROUND, TileType.GROUND, TileType.GROUND, TileType.GROUND, TileType.GROUND],
+    ]
+
+    const actionsMap: ActionsMap = [
+        [ActionType.NONE, ActionType.NONE, ActionType.NONE, ActionType.NONE, ActionType.NONE, ActionType.NONE, ActionType.NONE, ActionType.NONE],
+        [ActionType.NONE, ActionType.NONE, ActionType.NONE, ActionType.NONE, ActionType.NONE, ActionType.NONE, ActionType.NONE, ActionType.NONE],
+        [ActionType.NONE, ActionType.NONE, ActionType.NONE, ActionType.NONE, ActionType.NONE, ActionType.NONE, ActionType.NONE, ActionType.NONE],
+        [ActionType.NONE, ActionType.NONE, ActionType.NONE, ActionType.NONE, ActionType.NONE, ActionType.NONE, ActionType.NONE, ActionType.NONE],
+        [ActionType.NONE, ActionType.NONE, ActionType.NONE, ActionType.NONE, ActionType.NONE, ActionType.NONE, ActionType.NONE, ActionType.NONE],
+        [ActionType.NONE, ActionType.NONE, ActionType.NONE, ActionType.NONE, ActionType.NONE, ActionType.NONE, ActionType.NONE, ActionType.NONE],
+        [ActionType.NONE, ActionType.NONE, ActionType.NONE, ActionType.NONE, ActionType.NONE, ActionType.NONE, ActionType.WIN, ActionType.NONE],
+        [ActionType.NONE, ActionType.NONE, ActionType.NONE, ActionType.NONE, ActionType.NONE, ActionType.NONE, ActionType.NONE, ActionType.NONE],
+    ]
+
     return (
         <div>
-            <GameComponent />
+            <GameComponent levelTitle='Teste' totalActions={4} maps={{
+                gameTileMap,
+                actionsMap
+            }} />
         </div>
 
     );
