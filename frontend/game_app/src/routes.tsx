8f7a9a7d07a09046bfbc715090e57d789ed107b2
Vitor Chaves <vitorclcoelho@protonmail.com>
2024-10-09 22:33:11 -0300

    Adicionando página de validação de magic link

--- frontend/game_app/src/routes.tsx
+++ frontend/game_app/src/routes.tsx
@@ -1,33 +1,42 @@
 import { createBrowserRouter } from "react-router-dom";
 import HomePage from "./pages/Home";
-// import AccessPage from "./pages/Access";
-// import sendMagicLinkAction from "./actions/sendMagicLink";
-// import AccessConfirmPage from "./pages/AccessConfirm";
-// import LastGamePage from "./pages/LastGame";
+import AccessPage from "./pages/Access";
+import sendMagicLinkAction from "./actions/sendMagicLink";
+import AccessConfirmPage from "./pages/AccessConfirm";
+import LastGamePage from "./pages/LastGame";
 import GameStartPage from "./pages/GameStart";
 import GameExamplePage from "./pages/GameExample";
+import MagicLinkValidatePage from "./pages/MagicLinkValidate";
+import validateMagicLink from "./loaders/validateMagicLink.ts";
+import MagicLinkValidateErrorBoundary from "./components/MagicLinkValidateErrorBoundary.tsx";
 
 const router = createBrowserRouter([
     {
         path: "/",
         children: [
             {
-                path: "/",
+				index: true,
                 element: <HomePage />,
             },
-            // {
-            //     path: "access",
-            //     element: <AccessPage />,
-            //     action: sendMagicLinkAction,
-            // },
-            // {
-            //     path: "access-confirm",
-            //     element: <AccessConfirmPage />,
-            // },
-            // {
-            //     path: "last-game",
-            //     element: <LastGamePage />,
-            // },
+            {
+                path: "access",
+                element: <AccessPage />,
+                action: sendMagicLinkAction,
+            },
+            {
+                path: "access-confirm",
+                element: <AccessConfirmPage />,
+            },
+			{
+				path: "magic-link/validate",
+				element: <MagicLinkValidatePage />,
+				loader: validateMagicLink,
+				errorElement: <MagicLinkValidateErrorBoundary />
+			},
+            {
+                path: "last-game",
+                element: <LastGamePage />,
+            },
             {
                 path: 'game-start',
                 element: <GameStartPage/>
